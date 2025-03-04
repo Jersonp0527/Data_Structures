e9e3f641787e84c62c25681f1c3a22a74bc11efb
Jersonp0527 <jersonp2003@gmail.com>
2025-03-04 17:27:59 -0500

    Actualizar ejercicio1 para el parcial 2

--- Parcial 2/ejercicio1.py
+++ Parcial 2/ejercicio1.py
@@ -1,18 +1,30 @@
+# Diccionario para contar la cantidad de productos por equipo
 diccCant = {}
+
+# Diccionario para almacenar los productos únicos por equipo
 diccSet = {}
 
+# Bucle infinito para leer las entradas del usuario
 while(1):
-    line = input()
+    line = input()  # Leer una línea de entrada
     
     if(line == '#'):
-        break
+        break  # Si la línea es '#', salir del bucle
     
+    # Dividir la línea en equipo (eq), agente (ag) y producto (pr)
     eq, ag, pr = line.split()
     
+    # Incrementar el contador de productos para el equipo
     diccCant[eq] = diccCant.get(eq, 0) + 1
+    
+    # Inicializar el conjunto de productos para el equipo si no existe
     diccSet[eq] = diccSet.get(eq, set())
+    
+    # Agregar el producto al conjunto de productos del equipo
     diccSet[eq].add(pr)
     
+# Iterar sobre los equipos y sus conjuntos de productos
 for eq, pres in diccSet.items():
-    if(len(pres)>=3):
+    # Si el equipo tiene 3 o más productos únicos, imprimir el equipo y su contador de productos
+    if(len(pres) >= 3):
         print(eq, diccCant[eq])
